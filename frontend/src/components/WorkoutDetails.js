3079649a8e3dbb6302c8ed27b49d166376548cd4
aditishaktawat <146921675+aditishaktawat@users.noreply.github.com>
2023-10-13 00:26:39 +0530

    WorkoutDetails.js

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -7,7 +7,7 @@ const WorkoutDetails = ({workout }) => {
   const {dispatch} = useWorkoutsContext()
 
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
+    const response = await fetch('https://vercel.com/aditis-projects-0bcee96b/fitness-buddy-backend/45igsdyDNosUWQX8gi7DzC8VKZgY/api/workouts/' + workout._id, {
       method: 'DELETE'
     })
     const json = await response.json()
